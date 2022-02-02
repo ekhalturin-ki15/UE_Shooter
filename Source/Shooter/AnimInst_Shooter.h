4d12d04e50dc3c102a1d287baa141cfea183465a
ekhalturin-ki15 <1tigrin@mail.ru>
2022-02-03 01:49:34 +0700

    Post code cpp

---
+++ Source/Shooter/AnimInst_Shooter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "AnimInst_Shooter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTER_API UAnimInst_Shooter : public UAnimInstance
+{
+	GENERATED_BODY()
+	
+};
